11a167bea7c912d85c9f1bde1c9d0f0800b4096b
Om Sable <omsable214@gmail.com>
2025-11-12 21:46:15 +0530

    Create input_code.py

---
+++ backend/test/input_code.py
@@ -0,0 +1,12 @@
+"""
+Run the training command
+python hdva_agent.py --train
+
+After training, analyze a file
+python hdva_agent.py --file input_code.py
+"""
+
+def greet(name):
+    print("Hello", name)
+
+greet("HDVA")
